9057ce47a0a68fc719d57ac82d4184554880bf58
archpoint <edvard.holst@gmail.com>
2016-12-03 09:58:37 +0100

    Updated version in sempahor version checker script

--- semaphor/semaphor_version_checker.py
+++ semaphor/semaphor_version_checker.py
@@ -2,7 +2,7 @@ from urllib import urlopen
 from bs4 import BeautifulSoup
 import re
 
-semaphor_version_site = urlopen('https://spideroak.com/semaphor/versioncheck/1.1.7/linux/x64').read()
+semaphor_version_site = urlopen('https://spideroak.com/semaphor/versioncheck/1.2.1/linux/x64').read()
 output = BeautifulSoup(semaphor_version_site, "html.parser")
 versioninfo = output.find('div', 'cms-ribbon columns-one-ribbon versioncheck')
 for check in output.findAll('p', attrs={'class':'red'}):
